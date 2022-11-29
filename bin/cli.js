fdfffb461ffc3cbc368bf513ac623506944bed0d
Ce'Niyah Ellison <69821183+cre24@users.noreply.github.com>
2022-11-29 16:54:30 -0500

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,7 @@
 // Import statements
 import {roll} from '../lib/roll.js';
 import minimist from "minimist";
+
 //use minimist function to create argv variable
 const args = minimist(process.argv.slice(2))
 
